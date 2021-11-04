299a8c68c2b37eb9fa6f516440c44b9f1930502f
Domantas Jurkus <domantj@amazon.com>
2021-11-04 12:18:48 +0000

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'caa7a2edbb78c77bbd72b291cb0838bf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd2243b20-2ab3-474f-aede-6225ce53e800'
+};
